514418f7b2ce584ce10717545973002c700e038b
Eddie Franco <eddiefranco626@gmail.com>
2019-01-08 21:17:06 -0600

    added in react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Stats from './components/Stats'
+import Header from './components/layout/Header'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/stats" component={Stats} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Stats.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Stats extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Stats - hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default Stats
