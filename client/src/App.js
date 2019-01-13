b8d6476b233cda4b68fd4943608b8419d7f93254
Eddie Franco <eddiefranco626@gmail.com>
2019-01-13 15:46:03 -0600

    setting up style with material ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Stats from './components/Stats'
 import Header from './components/layout/Header'
-
 import './App.css';
 
 class App extends Component {
@@ -13,7 +12,7 @@ class App extends Component {
         <div className="App">
           <div className="container">
             <Header />
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Home} />            
             <Route exact path="/stats" component={Stats} />
           </div>
         </div>
--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export class Stats extends Component {
+class Stats extends Component {
   render() {
     return (
       <div>
