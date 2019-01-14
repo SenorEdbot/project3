f610da01b2139535281545bfa6423c4dc95ca95e
Jason Michael <michael.jason@outlook.com>
2019-01-13 20:35:40 -0600

    added auth0

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 import Game from './game'
 
 export class Home extends Component {
+  login() {
+    this.props.auth.login();
+  }
   render() {
+    const { isAuthenticated } = this.props.auth;
     return (
       <div>
-        <Game />
+        {
+          isAuthenticated() && (
+            <Game />
+          )
+        }
+        {
+          !isAuthenticated() && (
+            <h4>
+              You are not logged in! Please{' '}
+              <a
+                style={{ cursor: 'pointer' }}
+                onClick={this.login.bind(this)}
+              >Log in</a>
+              {' '}to continue.
+            </h4>
+          )
+        }
       </div>
     )
   }
