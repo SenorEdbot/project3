5f7ae3be96f100e87f938e0c15e80202ebc6b88a
Jason Michael <michael.jason@outlook.com>
2019-01-16 16:14:55 -0600

    fixed server not sending client build

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const cookieParser = require('cookie-parser');
-const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const server = express();
+
+// Serve static files from the React app
+server.use(express.static(path.join(__dirname, 'client/build')));
 
 // Middleware
 server.use(logger('dev'));
@@ -17,40 +20,20 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/midWaste";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
-const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
-server.use('/', indexRouter);
 server.use('/api', apiRouter);
 
-// Catch 404 and forward to error handler
-server.use((req, res, next) => next(createError(404)));
-
-// TODO: need to catch bad routes with '*'? (Catch 404 above might already do this)
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // Error handler
 server.use(function(err, req, res) {
   // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // TODO: render an error page (server-side) if desired
 });
-
-// let testObj = {
-//   name: "eddie",
-//   maxTimeSurvived: 7,
-//   maxDifficulty: 1,
-//   maxEnemiesKilled: 14,
-//   maxHealth: 80,
-//   maxShotsFired: 700,
-//   maxAccuracy: 75,
-//   historyTimeSurvived: [4, 2],
-//   historyDifficulty: [1,2],
-//   historyEnemiesKilled: [6, 5],
-//   historyHealth: [80, 70],
-//   historyShotsFired: [33, 16],
-//   historyAccuracy: [45, 21],
-//   friends: ['_10389382','_1038342'],
-// }
 
 module.exports = server;
