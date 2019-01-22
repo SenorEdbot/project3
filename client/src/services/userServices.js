dd0da426b3a3118e143660bb30185d793df5cba0
Eddie Franco <eddiefranco626@gmail.com>
2019-01-22 17:52:09 -0600

    added friend dropdown

--- client/src/services/userServices.js
+++ client/src/services/userServices.js
@@ -2,5 +2,6 @@ import api from './api'
 
 export default {
   saveUserStats: (username, userData) => api.post('/users/' + username, userData),
-  getUserByUsername: username => api.get('/users/' + username)
+  getUserByUsername: username => api.get('/users/' + username),
+  getAllUsers: () => api.get('/users/')
 }
